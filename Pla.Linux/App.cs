affa15acdad8b77f1f4422124af0d788701408e7
Pawel Galezowski <pawel.galezowski@tfbank.pl>
2020-12-14 21:20:17 +0100

    fixes

--- Pla.Linux/App.cs
+++ Pla.Linux/App.cs
@@ -2,16 +2,16 @@ using Gtk;
 using System;
 using SkiaSharp.Views.Gtk;
 using Pla.Lib;
+using Pla.Shared;
 
 namespace Pla.Linux
 {
-    public class App : Window {
+    public class App : Window, IEngine {
 
-        private SKDrawingArea drawingArea;
+        private SKDrawingArea _sk = new SKDrawingArea();
 
-        public App(IContext context) : base("Center")
+        public App(IContext ctx) : base("Center")
         {
-
             SetDefaultSize(250, 200);
             SetPosition(WindowPosition.Center);
             
@@ -20,8 +20,12 @@ namespace Pla.Linux
             VBox vbox = new VBox(false, 5);
             HBox hbox = new HBox(true, 3);
             
-            var engine = new Engine();
-            drawingArea = new SKDrawingArea();
+            ctx.Init(this);
+
+            var sw = new SkiaWrapper(ctx);
+            _sk.PaintSurface += (sender, args) => sw.OnSkOnPaintSurface(sender, args);
+            _sk.Touch += (sender, args) => sw.OnTouch(sender, args);
+            _sk.EnableTouchEvents = true;
             
             Add(drawingArea);
 
@@ -37,6 +41,8 @@ namespace Pla.Linux
             Application.Run();
         }
 
-       
+        public void RequestRefresh(){
+            this._sk.InvalidateSurface();
+        }
     }
 }
